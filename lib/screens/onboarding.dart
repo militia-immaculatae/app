df33f83347df9ca2501bdb81d5a400cc27a8c41b
SB2-David\david <david.wetter@outlook.com>
2020-12-14 20:02:00 +0100

    Create Flutter Project

---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'package:argon_flutter/constants/Theme.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/onboard-background.png"),
+                  fit: BoxFit.cover))),
+      Padding(
+        padding:
+            const EdgeInsets.only(top: 73, left: 32, right: 32, bottom: 16),
+        child: Container(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 48.0),
+                      child: Text.rich(TextSpan(
+                        text: "Militia Immaculatae",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w600),
+                      )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24.0),
+                      child: Text(
+                          "Du willst nichts mehr von der Militia Immaculatae verpassen?",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w200)),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      textColor: ArgonColors.text,
+                      color: ArgonColors.secondary,
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 12, bottom: 12),
+                          child: Text("LOS GEHTS",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0))),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    ]));
+  }
+}
